7495d2541724cc67f76f56744c777da73991e35d
Harry Schwartz <hello@harryrschwartz.com>
2015-09-21 09:08:59 -0400

    Format REPL output as Blueprint
    Display function and macro literals in a neatly formatted way.

--- lib/repl.rb
+++ lib/repl.rb
@@ -6,13 +6,25 @@ module Blueprint
     def exec
       print "> "
       while input = gets
-        puts interpreter.eval(input)
+        puts format(interpreter.eval(input))
         print "> "
       end
       puts
     end
 
     private
+
+    def format(expr)
+      if expr.is_a?(Array)
+        "(#{expr.map { |elt| format(elt) }.join(" ")})"
+      elsif expr.is_a?(Closure)
+        "#<lambda #{format(expr.variables)} -> #{format(expr.body)}>"
+      elsif expr.is_a?(Macro)
+        "#<macro #{format(expr.variables)} -> #{format(expr.body)}>"
+      else
+        expr.to_s
+      end
+    end
 
     def interpreter
       @_interpreter ||= Interpreter.new
