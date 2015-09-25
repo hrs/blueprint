80762affa7bfddce6ac374d445a9372e056581a3
Harry Schwartz <hello@harryrschwartz.com>
2015-09-25 15:47:26 -0400

    Add helper for Parser tests
    Removing some redundancy with an expect_parsed_value_of method.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,94 +2,74 @@ require "spec_helper"
 
 describe Blueprint::Parser do
   describe "#parse" do
+    def expect_parsed_value_of(expression)
+      expect(Blueprint::Parser.new(expression).parse)
+    end
+
     it "handle integers" do
-      parser = Blueprint::Parser.new("123")
-      expect(parser.parse).to eq([123])
+      expect_parsed_value_of("123").to eq([123])
     end
 
     it "handle floats" do
-      parser = Blueprint::Parser.new("-0.123")
-      expect(parser.parse).to eq([-0.123])
+      expect_parsed_value_of("-0.123").to eq([-0.123])
     end
 
     it "handle symbols" do
-      parser = Blueprint::Parser.new("abc")
-      expect(parser.parse).to eq([:abc])
+      expect_parsed_value_of("abc").to eq([:abc])
     end
 
     it "handle strings" do
-      parser = Blueprint::Parser.new("\"abc\"")
-      expect(parser.parse).to eq(["abc"])
+      expect_parsed_value_of("\"abc\"").to eq(["abc"])
     end
 
     it "escapes string characters" do
-      parser = Blueprint::Parser.new("\"a\\bc\"")
-      expect(parser.parse).to eq(["a\\bc"])
+      expect_parsed_value_of("\"a\\bc\"").to eq(["a\\bc"])
     end
 
     it "handle math sigils in symbols" do
-      parser = Blueprint::Parser.new("*")
-      expect(parser.parse).to eq([:*])
+      expect_parsed_value_of("*").to eq([:*])
     end
 
     it "handles ? in symbol names" do
-      parser = Blueprint::Parser.new("null?")
-      expect(parser.parse).to eq([:null?])
+      expect_parsed_value_of("null?").to eq([:null?])
     end
 
     it "handles ! in symbol names" do
-      parser = Blueprint::Parser.new("set!")
-      expect(parser.parse).to eq([:set!])
+      expect_parsed_value_of("set!").to eq([:set!])
     end
 
     it "handles booleans" do
-      parser = Blueprint::Parser.new("true")
-      expect(parser.parse).to eq([true])
-
-      parser = Blueprint::Parser.new("false")
-      expect(parser.parse).to eq([false])
+      expect_parsed_value_of("true").to eq([true])
+      expect_parsed_value_of("false").to eq([false])
     end
 
     it "handles empty lists" do
-      parser = Blueprint::Parser.new("()")
-      expect(parser.parse).to eq([[]])
-
-      parser = Blueprint::Parser.new("( )")
-      expect(parser.parse).to eq([[]])
+      expect_parsed_value_of("()").to eq([[]])
+      expect_parsed_value_of("( )").to eq([[]])
     end
 
     it "has a shortcut for quoting" do
-      parser = Blueprint::Parser.new("'()")
-      expect(parser.parse).to eq([[:quote, []]])
-
-      parser = Blueprint::Parser.new("'(a b c)")
-      expect(parser.parse).to eq([[:quote, [:a, :b, :c]]])
+      expect_parsed_value_of("'()").to eq([[:quote, []]])
+      expect_parsed_value_of("'(a b c)").to eq([[:quote, [:a, :b, :c]]])
     end
 
     it "parses the quasiquote/unquote/unquote-splicing symbols" do
-      parser = Blueprint::Parser.new("`4")
-      expect(parser.parse).to eq([[:quasiquote, 4]])
-
-      parser = Blueprint::Parser.new(",4")
-      expect(parser.parse).to eq([[:unquote, 4]])
-
-      parser = Blueprint::Parser.new(",@4")
-      expect(parser.parse).to eq([[:"unquote-splicing", 4]])
+      expect_parsed_value_of("`4").to eq([[:quasiquote, 4]])
+      expect_parsed_value_of(",4").to eq([[:unquote, 4]])
+      expect_parsed_value_of(",@4").to eq([[:"unquote-splicing", 4]])
     end
 
     it "handle simple s-expressions" do
-      parser = Blueprint::Parser.new("(+ 1 2)")
-      expect(parser.parse).to eq([[:+, 1, 2]])
+      expect_parsed_value_of("(+ 1 2)").to eq([[:+, 1, 2]])
     end
 
     it "handle nested s-expressions" do
-      parser = Blueprint::Parser.new("(+ (- 4 3) 2)")
-      expect(parser.parse).to eq([[:+, [:-, 4, 3], 2]])
+      expect_parsed_value_of("(+ (- 4 3) 2)").to eq([[:+, [:-, 4, 3], 2]])
     end
 
     it "handles conditionals" do
-      parser = Blueprint::Parser.new("(cond ((== 1 2) 3) (else 4))")
-      expect(parser.parse).to eq([[:cond, [[:==, 1, 2], 3], [:else, 4]]])
+      expect_parsed_value_of("(cond ((== 1 2) 3) (else 4))").
+        to eq([[:cond, [[:==, 1, 2], 3], [:else, 4]]])
     end
   end
 end
