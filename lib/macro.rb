e1ed1d06e4429c34b9e4c7a1fef52842ff9edfeb
Harry Schwartz <hello@harryrschwartz.com>
2016-04-01 11:14:12 -0400

    Define Macro as a class, not a Struct
    The attributes of classes derived from Structs are mutable (that is,
    they're defined as `attr_accessor`s). We never want to mutate the
    attributes of a macro, so we'll just use a regular ol' class instead.

--- lib/macro.rb
+++ lib/macro.rb
@@ -1,10 +1,19 @@
 module Blueprint
-  Macro = Struct.new(:variables, :body) do
+  class Macro
+    attr_reader :variables, :body
+
+    def initialize(variables, body)
+      @variables = variables
+      @body = body
+    end
+
     def expand(bindings, parent_frame)
       frame = Frame.new(parent_frame)
-      variables.zip(bindings).each do |var, val|
-        frame.define(var, val)
+
+      variables.zip(bindings).each do |variable, binding|
+        frame.define(variable, binding)
       end
+
       Closure.new([], body, frame)
     end
   end
